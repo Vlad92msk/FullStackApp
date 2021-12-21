74c03958be02ca333c353163b632ffbfcac829b8
firsovvs <7513325Qq>
2021-12-21 18:44:48 +0300

    CLIENT--Cosmo[ * Сделан компонент Image.tsx (передавать необходимо только форматы .vebp и .avif) * Доработан компонент Section.tsx (теперь он использует созданный Image.tsx) * Создана утилита createString для создания строки ]

--- src/client/shared/components/Section/Section.tsx
+++ src/client/shared/components/Section/Section.tsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import { makeCn } from '@shared/utils'
-import styles from './Section.module.scss'
 import { classnames } from '@bem-react/classnames'
+import { Image, ImageType } from '@shared/components/Image'
+import styles from './Section.module.scss'
 
 const cn = makeCn('Base', styles)
 
 type SectionType = {
   className?: string
-  bcgImg?: {
-    project: string
-    imgName: string
-  }
+  bcgImg?: ImageType
+  imgClassName?: string
   noPaddingLeft?: boolean
   noPaddingRight?: boolean
   noPaddingTop?: boolean
   noPaddingBottom?: boolean
 }
 export const Section: React.FC<SectionType> = React.memo((
-  { bcgImg, className, noPaddingLeft, noPaddingRight, noPaddingTop, noPaddingBottom, children }
+  { bcgImg, className, imgClassName, noPaddingLeft, noPaddingRight, noPaddingTop, noPaddingBottom, children }
 ) => {
-  const paddingLeft = noPaddingLeft && { paddingLeft: 0 }
-  const paddingRight = noPaddingRight && { paddingRight: 0 }
-  const paddingTop = noPaddingTop && { paddingTop: 0 }
-  const paddingBottom = noPaddingBottom && { paddingBottom: 0 }
+
+  const paddings = [
+    noPaddingLeft && { paddingLeft: 0 },
+    noPaddingRight && { paddingRight: 0 },
+    noPaddingTop && { paddingTop: 0 },
+    noPaddingBottom && { paddingBottom: 0 }
+  ].reduce((acc, item) => item ? {...acc, ...item} : acc, {})
 
   return (
     <section
       className={classnames(cn(), className)}
-      style={{ ...paddingLeft, ...paddingRight, ...paddingTop, ...paddingBottom }}
+      style={{...paddings}}
     >
-      {bcgImg && <img className={cn('Img')} src={`/resources/images/${bcgImg.project}/${bcgImg.imgName}`} alt='' />}
+      { bcgImg && <Image className={imgClassName} path={bcgImg.path} sizePriority={bcgImg.sizePriority }/> }
       {children}
     </section>
   )
