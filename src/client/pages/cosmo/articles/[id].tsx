dd252e541d090fc0be8d304027518216fe6a1852
firsovvs <7513325Qq>
2021-12-30 17:23:36 +0300

    CLIENT--Cosmo[ * Правильно дообернуты оставшиеся страницы ]

--- src/client/pages/cosmo/articles/[id].tsx
+++ src/client/pages/cosmo/articles/[id].tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { Page } from '@shared/components/page'
 
 const Article: NextPage = () => {
   const routers = useRouter()
-  return (<div>{routers.query.id}</div>)
+  return (<Page subTitle={'Cosmo'}>{routers.query.id}</Page>)
 }
 
 export default Article
--- src/client/pages/cosmo/index.tsx
+++ src/client/pages/cosmo/index.tsx
@@ -1,15 +1,12 @@
 import { NextPage } from 'next'
-import { AuthGuard } from '@shared/containers/AuthGuard'
 import { App } from '~client/projects/cosmo/containers/App'
 import { Page } from '@shared/components/page'
 
 
 const Index: NextPage = () => (
-  <AuthGuard>
-    <Page subTitle={'Cosmo'}>
-      <App />
-    </Page>
-  </AuthGuard>
+  <Page subTitle={'Cosmo'}>
+    <App />
+  </Page>
 )
 
 export default Index
