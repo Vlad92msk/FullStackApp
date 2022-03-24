6546222a303fcce156524fa3d95b187c30814f9e
firsovvs <7513325Qq>
2022-03-24 18:10:35 +0300

    CLIENT--social [ * Продолжаю верстку * сделан компонент AnimationToggleComponent ]

--- src/client/projects/social/containers/Profile/components/PhotoCard/PhotoCard.tsx
+++ src/client/projects/social/containers/Profile/components/PhotoCard/PhotoCard.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion, useAnimation } from 'framer-motion'
 
 import { makeCn } from '@client_shared/utils'
 import { Text } from '@client_shared/components/Text'
@@ -15,6 +15,7 @@ import { Modal } from '@client/shared/components/Modal'
 import { useBooleanState } from '@client/shared/hooks'
 import { TextInput } from '@client/shared/components/TextInput'
 import { AreaInput } from '@client/shared/components/AreaInput'
+import { AnimationToggleComponent } from '@client/shared/components/AnimationToggleComponent'
 
 const cn = makeCn('PhotoCard', styles)
 
@@ -44,16 +45,21 @@ export const PhotoCard: React.FC<PhotoCardType> = React.memo((props) => {
     authorName,
     title
   } = props
-  const [open, handleOpen, handleClose] = useBooleanState(false)
+  // const [open, handleOpen, handleClose] = useBooleanState(false)
   const [comment, setComment] = useState(null)
   const [isHover, setHover] = useState(null)
-  console.log('isHover', isHover)
 
   return (
-    <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className={cn()}>
-      <div className={cn('Row')}>
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-          <ButtonBox onClick={handleOpen} className={cn('Photo')}>
+    <AnimationToggleComponent
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={cn()}
+    >
+      <div style={{
+        // width: open ? '50%' : '100%'
+      }}>
+        <div className={cn('Row')}>
+          <ButtonBox className={cn('Photo')}>
             <Image path={{
               img: 'ava',
               project: 'social'
@@ -76,62 +82,54 @@ export const PhotoCard: React.FC<PhotoCardType> = React.memo((props) => {
               </motion.div>
             )}
           </AnimatePresence>
+          <AnimatePresence exitBeforeEnter initial={false}>
+            {isHover && (
+              <motion.div
+                initial={initialTranslateX}
+                animate={animateTranslateX}
+                exit={initialTranslateX}
+                transition={transition}
+                className={cn('ButtonsGroup')}
+              >
+                <div className={cn('Button')}>
+                  <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'heart'} fill={'redRose40'} />
+                  <Text className={cn('ButtonText')} size={'2'} children={likeCount || 0} />
+                </div>
+                <div className={cn('Button')}>
+                  <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'dislike'} fill={'bluePrimrose50'} />
+                  <Text className={cn('ButtonText')} size={'2'} children={disLikeCounts || 0} />
+                </div>
+                <div className={cn('Button')}>
+                  <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'message-square'}
+                        fill={'bluePrimrose50'} />
+                  <Text className={cn('ButtonText')} size={'2'} children={commentsCount || 0} />
+                </div>
+                <div className={cn('Button')}>
+                  <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'hash'} fill={'oldAsphalt40'} />
+                  <Text className={cn('ButtonText')} size={'2'} children={commentsCount || 0} />
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-        <AnimatePresence exitBeforeEnter initial={false}>
-          {isHover && (
-            <motion.div
-              initial={initialTranslateX}
-              animate={animateTranslateX}
-              exit={initialTranslateX}
-              transition={transition}
-              className={cn('ButtonsGroup')}
-            >
-              <div className={cn('Button')}>
-                <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'heart'} fill={'redRose40'} />
-                <Text className={cn('ButtonText')} size={'2'} children={likeCount || 0} />
-              </div>
-              <div className={cn('Button')}>
-                <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'dislike'} fill={'bluePrimrose50'} />
-                <Text className={cn('ButtonText')} size={'2'} children={disLikeCounts || 0} />
-              </div>
-              <div className={cn('Button')}>
-                <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'message-square'} fill={'bluePrimrose50'} />
-                <Text className={cn('ButtonText')} size={'2'} children={commentsCount || 0} />
-              </div>
-              <div className={cn('Button')}>
-                <Icon className={cn('ButtonIcon')} size={'ordinary'} icon={'hash'} fill={'oldAsphalt40'} />
-                <Text className={cn('ButtonText')} size={'2'} children={commentsCount || 0} />
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
+
+        {authorName && (
+          <div className={cn('AuthorRow')}>
+            <div className={cn('AuthorImg')}>
+              <Image sizePriority={open ? 'contain' : 'cover'} path={{
+                img: 'ava',
+                project: 'social'
+              }} />
+            </div>
+            <Text className={cn('AuthorName')} children={authorName} size={'1'} />
+          </div>
+        )}
       </div>
 
-      {authorName && (
-        <div className={cn('AuthorRow')}>
-          <div className={cn('AuthorImg')}>
-            <Image sizePriority={'cover'} path={{
-              img: 'ava',
-              project: 'social'
-            }} />
-          </div>
-          <Text className={cn('AuthorName')} children={authorName} size={'1'} />
-        </div>
-      )}
-      <Modal open={open} onClose={handleClose} className={cn('Modal')}>
-        <div className={cn('ModalPhotoContainer')}>
-          <Image
-            sizePriority={'contain'}
-            path={{
-              img: 'ava',
-              project: 'social'
-            }}
-          />
-        </div>
-        <div className={cn('Chat')}>
-          <AreaInput style={{ width: '100%', height: '100%' }} onChange={setComment} value={comment} />
-        </div>
-      </Modal>
-    </div>
+      <div>
+        <div>filters</div>
+        <AreaInput style={{ width: '100%', height: '100%' }} onChange={setComment} value={comment} />
+      </div>
+    </AnimationToggleComponent>
   )
 })
