dabb84c8a898f1561c6bc0c98a10d931db556551
firsovvs <7513325Qq>
2022-04-16 15:19:22 +0300

    CLIENT--social [ * продолжаю верстку ]

--- src/client/projects/social/containers_v2/Comments/Comments.tsx
+++ src/client/projects/social/containers_v2/Comments/Comments.tsx
@@ -2,26 +2,34 @@ import React, { useCallback, useState } from 'react'
 
 import { makeCn } from '@client_shared/utils'
 import { Text } from '@client/shared/components/Text'
+import { Button } from '@client/shared/components/Button'
 import { AreaInput, UserSmall } from '@client/projects/social/components'
 import { COMMENTS, CommentType } from '../Comments/data/comments.data'
-import { Actions, AnswerWrapper, CommentsWrapper } from '../Comments/components'
+import { Actions, AnswerWrapper, CommentsOpenType, CommentsWrapper } from '../Comments/components'
 import styles from './Comments.module.scss'
-import { Button } from '@client/shared/components/Button'
 
 const cn = makeCn('Comments', styles)
+
+
+/**
+ * По сколько комментариев отображать
+ */
+const COUNT_VISIBLE_COMMENTS = 1
+
 
 export type CommentsType = {
   isOpenComments: boolean
   width?: string
   commentsHeight?: string
+  openType?: CommentsOpenType
 }
-
 
 export const Comments: React.FC<CommentsType> = React.memo(
   ({
      isOpenComments,
      width,
-     commentsHeight
+     commentsHeight,
+     openType
    }) => {
     const [comment, setComment] = useState<string>(null)
     const [openCommentId, setOpenCommentId] = useState<number>(null)
@@ -29,13 +37,16 @@ export const Comments: React.FC<CommentsType> = React.memo(
     const [commentsStart, setCommentsStart] = useState(0)
     const [comments, setComments] = useState<CommentType[]>([])
 
+    /**
+     * Открыть/подгрузить комментарии
+     */
     const handleOpenComments = useCallback(() => {
-      setComments(prev => [...prev, ...COMMENTS.slice(commentsStart, commentsStart + 1)])
-      setCommentsStart(prev => prev + 1)
+      setComments(prev => [...prev, ...COMMENTS.slice(commentsStart, commentsStart + COUNT_VISIBLE_COMMENTS)])
+      setCommentsStart(prev => prev + COUNT_VISIBLE_COMMENTS)
     }, [commentsStart])
 
     return (
-      <CommentsWrapper width={width} isOpenComments={isOpenComments}>
+      <CommentsWrapper width={width} isOpenComments={isOpenComments} openType={openType}>
         <div className={cn('Filter')}>filters</div>
         <div className={cn('Container')}>
           <div className={cn('AuthorComment')}>
@@ -104,10 +115,11 @@ export const Comments: React.FC<CommentsType> = React.memo(
               onClick={handleOpenComments}
               color={'grey'}
               styleType={'rounded'}
+              disabled={comments.length === COMMENTS.length}
             >
               <Text
                 size={'1'}
-                children={`Показать комментарии [${comments.length}/${COMMENTS.length}]`}
+                children={`Показать ${comments.length ? 'еще' : `первые ${COUNT_VISIBLE_COMMENTS}`} [${comments.length}/${COMMENTS.length}]`}
               />
             </Button>
           </div>
@@ -115,3 +127,7 @@ export const Comments: React.FC<CommentsType> = React.memo(
       </CommentsWrapper>
     )
   })
+
+Comments.defaultProps = {
+  openType: 'horizontal'
+}
--- src/client/projects/social/containers_v2/Profile/components/ProfileLayoutWall/ProfileLayoutWall.tsx
+++ src/client/projects/social/containers_v2/Profile/components/ProfileLayoutWall/ProfileLayoutWall.tsx
@@ -3,6 +3,8 @@ import { makeCn } from '@client_shared/utils'
 import { UserSmall } from '@client/projects/social/components'
 import { Text } from '@client/shared/components/Text'
 import { Comments } from '@client/projects/social/containers_v2/Comments'
+import { Image } from '@client/shared/components/Image'
+import { Icon } from '@client/shared/components/Icon'
 import styles from './ProfileLayoutWall.module.scss'
 
 
@@ -18,7 +20,7 @@ type ProfileLayoutWallType = {
 export const ProfileLayoutWall: React.FC<ProfileLayoutWallType> = React.memo((props) => {
   const { userId } = props
 
-
+  const [isOpenComments, setOpenComments] = useState(null)
   return (
     <div className={cn()}>
       <div className={cn('Record')}>
@@ -34,8 +36,53 @@ export const ProfileLayoutWall: React.FC<ProfileLayoutWallType> = React.memo((pr
           <Text className={cn('Date')} size={'2'} children={'01/21/2002'} />
         </div>
         <div className={cn('Body')}>
-          <Text className={cn('RecordText')} children={'dwedwed'} />
-          <Comments width={'100%'} commentsHeight={'40vh'} isOpenComments={true} />
+          <div className={cn('Content')}>
+            <div className={cn('RecordAttach')}>
+              <Image className={cn('RecordAttachItem')} path={{ img: 'ava', project: 'social' }} sizePriority={'contain'} />
+            </div>
+            <Text className={cn('RecordText')} children={'dwedwed'} />
+            <div className={cn('ButtonsGroup')}>
+              <div className={cn('Button')} onClick={() => setOpenComments(prev => !prev)}>
+                <Icon
+                  className={cn('ButtonIcon')}
+                  size={'ordinary'}
+                  icon={'message-square'}
+                  fill={'bluePrimrose50'}
+                />
+                <Text
+                  className={cn('ButtonText')}
+                  size={'2'}
+                  children={234}
+                />
+              </div>
+              <div style={{ display: 'flex' }}>
+                <div className={cn('Button')}>
+                  <Icon
+                    className={cn('ButtonIcon')}
+                    size={'ordinary'}
+                    icon={'heart'}
+                    fill={'redRose40'}
+                  />
+                  <Text className={cn('ButtonText')} size={'2'} children={1} />
+                </div>
+                <div className={cn('Button')}>
+                  <Icon
+                    className={cn('ButtonIcon')}
+                    size={'ordinary'}
+                    icon={'dislike'}
+                    fill={'bluePrimrose50'}
+                  />
+                  <Text className={cn('ButtonText')} size={'2'} children={23} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <Comments
+            width={'100%'}
+            commentsHeight={'40vh'}
+            isOpenComments={isOpenComments}
+            openType={'vertical'}
+          />
         </div>
       </div>
     </div>
