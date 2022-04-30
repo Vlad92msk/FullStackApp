00b84f387c01df2fffdcbe976b360c531c4bf36a
firsovvs <7513325Qq>
2022-04-30 19:26:18 +0300

    CLIENT--social [ * продолжаю верстку ]

--- src/client/projects/social/containers_v3/App/App.tsx
+++ src/client/projects/social/containers_v3/App/App.tsx
@@ -2,20 +2,18 @@ import React from 'react'
 
 import { Section } from '@client_shared/components/Section'
 import { makeCn } from '@client_shared/utils'
+import { Text } from '@client_shared/components/Text'
+import { ButtonBox } from '@client_shared/components/ButtonBox'
+import { Icon } from '@client_shared/components/Icon'
 import { NavBar } from '../NavBar'
 import { UserMenu } from '../UserMenu'
 import styles from './App.module.scss'
-import { IconButton } from '@client/shared/components/IconButton'
-import { Text } from '@client/shared/components/Text'
-import { ButtonBox } from '@client/shared/components/ButtonBox'
-import { Icon } from '@client/shared/components/Icon'
 
 const cn = makeCn('Application', styles)
 type AppType = {
   pathname: string
 }
 export const App: React.FC<AppType> = React.memo(({ children, pathname }) => {
-
   return (
     <Section noPaddingRight className={cn()} bcgImg={{
       path: {
@@ -29,8 +27,8 @@ export const App: React.FC<AppType> = React.memo(({ children, pathname }) => {
       <NavBar pathname={pathname} />
       <ButtonBox className={cn('Chat')}>
         <Icon className={cn('ChatIcon')} icon={'message-square'} size={'ordinary'} />
-        <Text className={cn('ChatCount')} children={null} size={'7'}/>
+        <Text className={cn('ChatCount')} children={null} size={'7'} />
       </ButtonBox>
     </Section>
   )
-})
+}, (a, b) => a.pathname === b.pathname)
